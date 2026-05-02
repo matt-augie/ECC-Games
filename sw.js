4fb870fc138583cbae8c5912c9a2b232a527468b
matt-augie <matt.augustson@gmail.com>
2026-05-02 08:44:45 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wheel-v8';
+const CACHE = 'wheel-v9';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
